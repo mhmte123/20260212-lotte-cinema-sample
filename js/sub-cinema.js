f41f3403577e7e39b34ddb70807ac64556e06934
junhyoung624 <mjhyoung0624@gmail.com>
2026-03-03 17:46:10 +0900

    26.03.03 서브 페이지 수정

--- js/sub-cinema.js
+++ js/sub-cinema.js
@@ -19,4 +19,37 @@ mapClose.addEventListener("click", () => {
     mapClose.classList.remove("active");
     mapBtn.classList.remove("active");
     map.classList.remove("active");
-}) 
+})
+
+let theaterLoc = document.querySelectorAll(".theater-loc-list>li>a");
+let theaterContent = document.querySelectorAll(".theater-wrap .theater-list")
+
+
+console.log(theaterLoc);
+console.log(theaterContent);
+
+
+theaterLoc.forEach((loc, id) => {
+    loc.addEventListener("click", () => {
+        theaterLoc.forEach((l, i) => {
+            l.classList.remove("active");
+            theaterContent[i].classList.remove("active");
+        })
+        loc.classList.add("active");
+        theaterContent[id].classList.add("active");
+
+    })
+})
+
+let theaterList = document.querySelectorAll(".theater-list>li");
+console.log(theaterList);
+
+theaterList.forEach((list, id) => {
+    list.addEventListener("click", () => {
+        theaterList.forEach((li, i) => {
+            li.classList.remove("active");
+        })
+        list.classList.add("active");
+    })
+})
+
