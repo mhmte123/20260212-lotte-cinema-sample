fe3322f89ce0daa2afcc5c28cd599a166cc3f542
junhyoung624 <mjhyoung0624@gmail.com>
2026-03-05 16:00:45 +0900

    26.03.05 서브 페이지/헤더 푸터 수정

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ closeBtn.addEventListener("click", () => {
 
 let nav = document.querySelector("header")
 let logoHeight = document.querySelector("nav").offsetHeight;
-console.log(logoHeight);
+// console.log(logoHeight);
 // let sPos = document.documentElement.scrollTop;
 
 
@@ -23,4 +23,53 @@ document.addEventListener("scroll", function (e) {
     } else {
         nav.classList.remove("active");
     }
+})
+
+
+let mainMenu = document.querySelectorAll(".main-menu>li");
+// let fMain = document.querySelectorAll(".main-menu>li>a");
+let subMenu = document.querySelectorAll(".sub-menu");
+let subBg = document.querySelector(".sub-bg");
+
+let wWidth;
+
+function siteInit() {
+    wWidth = window.innerWidth;
+    console.log(wWidth);
+
+}
+siteInit();
+
+window.addEventListener("resize", () => {
+    siteInit();
+})
+
+mainMenu.forEach(main => {
+    main.addEventListener("mouseenter", (e) => {
+        if (wWidth > 768) {                         
+            // console.log(main.nextElementSibling);
+            
+            let mMenu = main.querySelector(".sub-menu");
+            // console.log(mMenu);
+            
+            if (mMenu !== null) {
+                mMenu.style.height = "100%";
+                subBg.classList.add("active");
+            }
+        }
+    })
+})
+
+mainMenu.forEach(main => {
+    main.addEventListener("mouseleave", (e) => {
+        if (wWidth > 768) {
+            // let mMenu = main.lastElementChild;
+            // console.log(mMenu);
+            let mMenu = main.querySelector(".sub-menu");
+            if (mMenu !== null) {
+                mMenu.style.height = "0";
+                subBg.classList.remove("active");
+            }
+        }
+    })
 })
--- js/sub-cinema.js
+++ js/sub-cinema.js
@@ -3,19 +3,22 @@ let mapClose = document.querySelector(".map-close-btn");
 let map = document.querySelector(".map");
 let mapCloseBtn = document.querySelector(".theater-name .map .close-btn")
 
-mapBtn.addEventListener("click", () => {
+mapBtn.addEventListener("click", (e) => {
+    e.preventDefault();
     map.classList.toggle("active");
     mapBtn.classList.add("active");
     mapClose.classList.add("active");
 })
 
-mapCloseBtn.addEventListener("click", () => {
+mapCloseBtn.addEventListener("click", (e) => {
+    e.preventDefault();
     map.classList.remove("active");
     mapClose.classList.remove("active");
     mapBtn.classList.remove("active");
 })
 
-mapClose.addEventListener("click", () => {
+mapClose.addEventListener("click", (e) => {
+    e.preventDefault();
     mapClose.classList.remove("active");
     mapBtn.classList.remove("active");
     map.classList.remove("active");
@@ -30,7 +33,8 @@ console.log(theaterContent);
 
 
 theaterLoc.forEach((loc, id) => {
-    loc.addEventListener("click", () => {
+    loc.addEventListener("click", (e) => {
+        e.preventDefault();
         theaterLoc.forEach((l, i) => {
             l.classList.remove("active");
             theaterContent[i].classList.remove("active");
@@ -45,7 +49,8 @@ let theaterList = document.querySelectorAll(".theater-list>li");
 console.log(theaterList);
 
 theaterList.forEach((list, id) => {
-    list.addEventListener("click", () => {
+    list.addEventListener("click", (e) => {
+        e.preventDefault();
         theaterList.forEach((li, i) => {
             li.classList.remove("active");
         })
@@ -58,12 +63,29 @@ let tabContent = document.querySelectorAll(".theater-info-sche-wrap .info-sche")
 console.log(theaterTab);
 
 theaterTab.forEach((tab, id) => {
-    tab.addEventListener("click", ()=>{
+    tab.addEventListener("click", (e)=>{
+        e.preventDefault();
         theaterTab.forEach((t, i) => {
-            t.classList.remove("active")
-            tabContent[i].classList.remove("active")
+            t.classList.remove("active");
+            tabContent[i].classList.remove("active");
         })
-        tab.classList.add("active")
-        tabContent[id].classList.add("active")
+        tab.classList.add("active");
+        tabContent[id].classList.add("active");
+    })
+})
+
+let calendarDate = document.querySelectorAll(".calendar-date>li");
+let movieList = document.querySelectorAll(".movie-time-wrap .movie-wrap")
+console.log(movieList);
+
+calendarDate.forEach((cal, id) => {
+    cal.addEventListener("click", (e)=>{
+        e.preventDefault();
+        calendarDate.forEach((c, i) =>{
+            c.classList.remove("active");
+            movieList[i].classList.remove("active");
+        })
+        cal.classList.add("active");
+        movieList[id].classList.add("active");
     })
 })
